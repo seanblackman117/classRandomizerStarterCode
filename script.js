3881ee1f3dca603970fea6c29087feeb3088b251
Sean <sean.a.blackman@student.uncommonhs.org>
2017-05-04 21:56:38 +0000

    zxsdcfghjkl

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
-
-
+ var ScriptEders = ["Sean", "Mustapha", "Syndra", "Julian", "Isaiah", "Brian G.", "Rosanique"];
+ var ScriptEducators = ["Julia", "Shea", "Scot", "Mr.Goett"];
+ 
+  $("#studentButton").click(function(){
+     var bob = ScriptEders[Math.floor(Math.random()*ScriptEders.length)]; 
+      $("#studentDisplay").html(bob);
+  });
+       
+   $("#teacherButton").click(function(){
+     var tim = ScriptEducators[Math.floor(Math.random()*ScriptEducators.length)]; 
+      $("#teacherDisplay").html(tim);
+  });
 });
